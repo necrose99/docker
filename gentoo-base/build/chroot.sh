6314b30147e14e7a9d00c64c704d1958637f052d
Mikael Jenkler <mikael@jenkler.com>
2015-08-14 18:34:27 +0200

    Added update script Updated source files for Gentoo base

--- gentoo-base/build/chroot.sh
+++ gentoo-base/build/chroot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -p update.sh gentoo-base/root/ &> /dev/null
 mount -t proc proc gentoo-base/proc/
 mount --rbind /dev/ gentoo-base/dev/
 mount --rbind /sys/ gentoo-base/sys/
@@ -7,3 +8,4 @@ mount --rbind /sys/ gentoo-base/sys/
 chroot gentoo-base/ /bin/bash
 
 umount -l gentoo-base/proc/ gentoo-base/dev/ gentoo-base/sys/
+rm gentoo-base/root/update.sh &> /dev/null
