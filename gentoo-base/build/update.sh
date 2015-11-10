53a5548bea1dc4e39686a3235d3665565044ff22
Mikael Jenkler <mikael@jenkler.com>
2015-11-10 11:49:57 +0100

    Updated gentoo base

--- gentoo-base/build/update.sh
+++ gentoo-base/build/update.sh
@@ -4,9 +4,9 @@ emerge --sync
 env-update
 source /etc/profile
 emerge -aDNquv world
-etc-update
 perl-cleaner --all
 python-updater
 emerge -av --depclean
 revdep-rebuild
 emerge -1av @preserved-rebuild
+etc-update
